11aa981a2660aba3dd504101f0681c4236adbe70
Jacob Parish <jacobsparish@gmail.com>
2022-11-30 23:23:12 -0600

    Add boilerplate from last year

---
+++ src/day00.py
@@ -0,0 +1,14 @@
+from aocd import numbers, submit
+
+
+def part_a() -> int:
+    raise NotImplementedError()
+
+
+def part_b() -> int:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    submit(part_a(), part="a", day=0, year=2022)
+    submit(part_b(), part="b", day=0, year=2022)
---
+++ typings/aocd.pyi
@@ -0,0 +1,22 @@
+from typing import List
+from requests.models import Response
+
+data: str
+lines: List[str]
+numbers: List[int]
+
+def get_data(
+    session: str = ...,
+    day: int = ...,
+    year: int = ...,
+    block: bool = ...,
+) -> str: ...
+def submit(
+    answer: int,
+    part: str = ...,
+    day: int = ...,
+    year: int = ...,
+    session: str = ...,
+    reopen: bool = ...,
+    quiet: bool = ...,
+) -> Response: ...
