f98d8151126290c57a04c408409c74795cef680a
Jacob Parish <jacobsparish@gmail.com>
2022-12-03 23:22:06 -0600

    Make things less verbose

--- src/day04.py
+++ src/day04.py
@@ -4,24 +4,18 @@ from typing import Tuple
 import utils
 
 
-def ranges_contain(range_tuple: Tuple[int, int, int, int]):
-    (x1, y1, x2, y2) = range_tuple
-    return (x1 >= x2 and y1 <= y2) or (x1 <= x2 and y1 >= y2)
-
-
 def part_a() -> int:
-    ranges = utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
-    return sum(map(ranges_contain, ranges))
-
-
-def ranges_overlap(range_tuple: Tuple[int, int, int, int]):
-    (x1, y1, x2, y2) = range_tuple
-    return x1 <= y2 and y1 >= x2
+    return sum(
+        (x1 >= x2 and y1 <= y2) or (x1 <= x2 and y1 >= y2)
+        for (x1, y1, x2, y2) in utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
+    )
 
 
 def part_b() -> int:
-    ranges = utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
-    return sum(map(ranges_overlap, ranges))
+    return sum(
+        x1 <= y2 and y1 >= x2
+        for (x1, y1, x2, y2) in utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
+    )
 
 
 if __name__ == "__main__":
