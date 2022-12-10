7fe8b887f05e9cb2b384b797d3eedaa5764d780c
Jacob Parish <jacobsparish@gmail.com>
2022-12-10 18:50:41 -0500

    Add example inputs

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from aocd import submit
 import importlib
 import argparse
+import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("day", type=int)
+    parser.add_argument("--test", action="store_true")
     args = parser.parse_args()
-    day_module = importlib.import_module(f"day{args.day:02d}")
-    submit(day_module.p1, part="a", day=args.day, year=2022)
-    submit(day_module.p2, part="b", day=args.day, year=2022)
+    if args.test:
+        sys.modules["aocd"] = __import__("mock_B")
+        day_module = importlib.import_module(f"day{args.day:02d}")
+    else:
+        day_module = importlib.import_module(f"day{args.day:02d}")
+        submit(day_module.p1, part="a", day=args.day, year=2022)
+        submit(day_module.p2, part="b", day=args.day, year=2022)
---
+++ src/test.py
@@ -0,0 +1,20 @@
+import importlib
+import argparse
+import sys
+
+
+class AocdMock:
+    def __init__(self, day: int):
+        with open(f"data/examples/day{day:02d}/input.txt", "r") as f:
+            self.data = f.read()
+        self.lines = self.data.splitlines()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("day", type=int)
+    args = parser.parse_args()
+    sys.modules["aocd"] = AocdMock(args.day)
+    day_module = importlib.import_module(f"day{args.day:02d}")
+    print("p1:", day_module.p1)
+    print("p2:", day_module.p2)
