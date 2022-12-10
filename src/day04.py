bbe904f111b62d978a341a05608afa4695bb7616
Jacob Parish <jacobsparish@gmail.com>
2022-12-10 18:56:11 -0500

    Update day 4 to new system

--- src/day04.py
+++ src/day04.py
@@ -1,21 +1,9 @@
-from aocd import lines, submit
+from aocd import lines
 import utils
 
 
-def part_a() -> int:
-    return sum(
-        (x1 >= x2 and y1 <= y2) or (x1 <= x2 and y1 >= y2)
-        for (x1, y1, x2, y2) in utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
-    )
-
-
-def part_b() -> int:
-    return sum(
-        x1 <= y2 and y1 >= x2
-        for (x1, y1, x2, y2) in utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
-    )
-
-
-if __name__ == "__main__":
-    submit(part_a(), part="a", day=4, year=2022)
-    submit(part_b(), part="b", day=4, year=2022)
+ranges = utils.parse_lines(lines, "{:d}-{:d},{:d}-{:d}")
+p1 = sum(
+    (x1 >= x2 and y1 <= y2) or (x1 <= x2 and y1 >= y2) for (x1, y1, x2, y2) in ranges
+)
+p2 = sum(x1 <= y2 and y1 >= x2 for (x1, y1, x2, y2) in ranges)
