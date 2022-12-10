ef4e18195a23adad0e5468fdb20a2416420c52d9
Jacob Parish <jacobsparish@gmail.com>
2022-12-10 18:51:38 -0500

    Update day 1 to new system

--- src/day01.py
+++ src/day01.py
@@ -1,18 +1,8 @@
-from aocd import lines, submit
+from aocd import lines
 import heapq
 import utils
 
 
-def part_a() -> int:
-    return max(sum(calories) for calories in utils.split_numbers(lines))
-
-
-def part_b() -> int:
-    return sum(
-        heapq.nlargest(3, (sum(calories) for calories in utils.split_numbers(lines)))
-    )
-
-
-if __name__ == "__main__":
-    submit(part_a(), part="a", day=1, year=2022)
-    submit(part_b(), part="b", day=1, year=2022)
+calorie_totals = [sum(calories) for calories in utils.split_numbers(lines)]
+p1 = max(calorie_totals)
+p2 = sum(heapq.nlargest(3, calorie_totals))
