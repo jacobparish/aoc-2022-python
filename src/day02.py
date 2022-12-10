ae45a5ca5dc29a0b288cf6e1c95127300a16f00c
Jacob Parish <jacobsparish@gmail.com>
2022-12-10 18:54:01 -0500

    Update day 2 to new system

--- src/day02.py
+++ src/day02.py
@@ -1,28 +1,19 @@
-from aocd import lines, submit
+from aocd import lines
 
 
-def calc_score_a(line):
-    [p1, p2] = line.split(" ")
-    p1num = "ABC".index(p1)
-    p2num = "XYZ".index(p2)
-    return 3 * ((p2num - p1num + 1) % 3) + p2num + 1
+def calc_score_1(line):
+    [abc, xyz] = line.split(" ")
+    num1 = "ABC".index(abc)
+    num2 = "XYZ".index(xyz)
+    return 3 * ((num2 - num1 + 1) % 3) + num2 + 1
 
 
-def part_a() -> int:
-    return sum(map(calc_score_a, lines))
+def calc_score_2(line):
+    [abc, xyz] = line.split(" ")
+    num1 = "ABC".index(abc)
+    result = "XYZ".index(xyz)
+    return 3 * result + (num1 + result - 1) % 3 + 1
 
 
-def calc_score_b(line):
-    [p1, p2] = line.split(" ")
-    p1num = "ABC".index(p1)
-    result = "XYZ".index(p2)
-    return 3 * result + (p1num + result - 1) % 3 + 1
-
-
-def part_b() -> int:
-    return sum(map(calc_score_b, lines))
-
-
-if __name__ == "__main__":
-    submit(part_a(), part="a", day=2, year=2022)
-    submit(part_b(), part="b", day=2, year=2022)
+p1 = sum(map(calc_score_1, lines))
+p2 = sum(map(calc_score_2, lines))
