157c1e5d43effc670b01baafd84fa2cf8ac74cb9
Jacob Parish <jacobsparish@gmail.com>
2022-12-10 18:58:59 -0500

    Update day 5 to new system

--- src/day05.py
+++ src/day05.py
@@ -1,45 +1,27 @@
-from typing import List
-from aocd import lines, submit
+from aocd import lines
 import utils
+from typing import List
 
 
-def parse_input(lines: List[str]):
-    crate_rows, instrs = utils.parse_line_groups(
-        lines, (None, "move {:d} from {:d} to {:d}")
-    )
+crate_rows, instrs = utils.parse_line_groups(
+    lines, (None, "move {:d} from {:d} to {:d}")
+)
 
-    ncrates = (len(crate_rows.pop()) + 1) // 4
+ncrates = (len(crate_rows.pop()) + 1) // 4
 
-    crates = [[] for _ in range(ncrates)]
+crates1 = [[] for _ in range(ncrates)]
+crates2 = [[] for _ in range(ncrates)]
 
-    for row in crate_rows:
-        for i, val in enumerate(row[1::4]):
-            if val != " ":
-                crates[i].append(val)
+for row in crate_rows:
+    for i, val in enumerate(row[1::4]):
+        if val != " ":
+            crates1[i].append(val)
+            crates2[i].append(val)
 
-    return crates, instrs
+for count, src, dst in instrs:
+    for i in range(count):
+        crates1[dst - 1].insert(0, crates1[src - 1].pop(0))
+        crates2[dst - 1].insert(i, crates2[src - 1].pop(0))
 
-
-def part_a() -> str:
-    crates, instrs = parse_input(lines)
-
-    for count, src, dst in instrs:
-        for _ in range(count):
-            crates[dst - 1].insert(0, crates[src - 1].pop(0))
-
-    return "".join(crate[0] for crate in crates)
-
-
-def part_b() -> str:
-    crates, instrs = parse_input(lines)
-
-    for count, src, dst in instrs:
-        for i in range(count):
-            crates[dst - 1].insert(i, crates[src - 1].pop(0))
-
-    return "".join(crate[0] for crate in crates)
-
-
-if __name__ == "__main__":
-    submit(part_a(), part="a", day=5, year=2022)
-    submit(part_b(), part="b", day=5, year=2022)
+p1 = "".join(crate[0] for crate in crates1)
+p2 = "".join(crate[0] for crate in crates2)
