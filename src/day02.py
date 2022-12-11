9263e7ca098c1cdd44aba5434a00e0b936c3d9e5
Jacob Parish <jacobsparish@gmail.com>
2022-12-11 00:52:11 -0500

    Update day02.py

--- src/day02.py
+++ src/day02.py
@@ -2,14 +2,14 @@ from aocd import lines
 
 
 def calc_score_1(line):
-    [abc, xyz] = line.split(" ")
+    abc, xyz = line.split(" ")
     num1 = "ABC".index(abc)
     num2 = "XYZ".index(xyz)
     return 3 * ((num2 - num1 + 1) % 3) + num2 + 1
 
 
 def calc_score_2(line):
-    [abc, xyz] = line.split(" ")
+    abc, xyz = line.split(" ")
     num1 = "ABC".index(abc)
     result = "XYZ".index(xyz)
     return 3 * result + (num1 + result - 1) % 3 + 1
